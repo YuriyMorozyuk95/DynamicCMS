22d5f05d2ad3b3631baaeab25e1cdf428b76e11a
MOROZIUK Yurii <Yurii.Moroziuk@gunvorgroup.com>
2020-09-13 12:58:32 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DynamicCMS.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
